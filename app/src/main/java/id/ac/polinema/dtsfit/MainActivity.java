929f1c35e3a8cf5c3da9375e05c6544e670c4afb
AgikFendiHarvyandha <agikfendih@gmail.com>
2019-12-02 16:47:57 +0700

    calory update

--- app/src/main/java/id/ac/polinema/dtsfit/MainActivity.java
+++ app/src/main/java/id/ac/polinema/dtsfit/MainActivity.java
@@ -11,17 +11,28 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+import java.util.Locale;
+
 import id.ac.polinema.dtsfit.adapters.CaloriesAdapter;
 import id.ac.polinema.dtsfit.fragments.CaloryFragment;
 import id.ac.polinema.dtsfit.fragments.ProfileFragment;
 import id.ac.polinema.dtsfit.fragments.SaveCaloryFragment;
+import id.ac.polinema.dtsfit.generator.ServiceGenerator;
 import id.ac.polinema.dtsfit.models.Calory;
+import id.ac.polinema.dtsfit.services.CaloryService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements
 		CaloryFragment.OnFragmentInteractionListener,
 		SaveCaloryFragment.OnFragmentInteractionListener {
 
 	private Profile profile;
+	private CaloryService caloryService;
 
 	// TODO: Definisikan CaloryService caloryService
 
@@ -33,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements
 		setSupportActionBar(toolbar);
 
 		Profile profile = Application.provideProfile();
+		caloryService = ServiceGenerator.createService(CaloryService.class);
 
 		// TODO: Instansiasi nilai caloryService dengan menggunakan ServiceGenerator
 
@@ -86,21 +98,43 @@ public class MainActivity extends AppCompatActivity implements
 
 	@Override
 	public void onCaloryFragmentCreated(final View view, final CaloriesAdapter adapter, final TextView caloryText) {
+		Call<List<Calory>> caloriesCall = caloryService.getCalories();
+		caloriesCall.enqueue(new Callback<List<Calory>>() {
+			@Override
+			public void onResponse(Call<List<Calory>> call, Response<List<Calory>> response) {
+				List<Calory> calories = response.body();
+				adapter.setCalories(calories);
+				// Tambahkan logic di baris ini untuk mengkalkulasi total calory
+				int total = 0;
+				for (Calory calory : calories){
+					total += calory.getCalory();
+				}
+				caloryText.setText(String.format(Locale.ENGLISH,  "Your calory %d cal", total));
+			}
+
+			@Override
+			public void onFailure(Call<List<Calory>> call, Throwable t) {
+				Snackbar.make(view, "Oops!", Snackbar.LENGTH_SHORT).show();
+			}
+		});
 		// TODO: Implementasikan load data calory
 	}
 
 	@Override
 	public void onAddCaloryButtonClicked() {
+		changeFragment(SaveCaloryFragment.newInstance(null));
         // TODO: Implementasikan aksi tombol tambah calory pada CaloryFragment
 	}
 
 	@Override
 	public void onCaloryClicked(Calory calory) {
+		changeFragment(SaveCaloryFragment.newInstance(calory));
         // TODO: Implementasikan aksi item calory ketika dipilih pada CaloryFragment
 	}
 
 	@Override
 	public void onSaveMenuClicked(final View view, Calory calory) {
+
 		// TODO: Implementasikan aksi ketika menu simpan ditekan pada SaveCaloryFragment
 	}
 }
--- app/src/main/java/id/ac/polinema/dtsfit/services/CaloryService.java
+++ app/src/main/java/id/ac/polinema/dtsfit/services/CaloryService.java
@@ -1,10 +1,25 @@
 package id.ac.polinema.dtsfit.services;
 
+import java.util.List;
+
+import id.ac.polinema.dtsfit.models.Calory;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public interface CaloryService {
 
-    // TODO: Definisikan service getCalories()
+    @GET("/AgikFendiHarvyandha/dts-fit/calories")
+    Call<List<Calory>> getCalories();
 
     // TODO: Definisikan service addCalory()
+    @POST("/AgikFendiHarvyandha/dts-fit/calories")
+    Call<Calory> addCalory(@Body Calory calory);
 
     // TODO: Definisikan service editCalory()
+    @PUT("/AgikFendiHarvyandha/dts-fit/calories/{id}")
+    Call<Calory> editCalory(@Path("id") int id, @Body Calory calory);
 }
